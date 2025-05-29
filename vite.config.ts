327d5ef95e03e795ecf9ef31e88aa7f0498053cd
tscj3490 <tamara.dev1227@gmail.com>
2025-05-28 22:21:47 -0400

    fix some error in the client part

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,8 @@ export default defineConfig({
   root: path.resolve(__dirname, "client"),
   build: {
     outDir: path.resolve(__dirname, "dist/public"),
-    emptyOutDir: true
+    emptyOutDir: true,
+    sourcemap: false
   }
 });
 
